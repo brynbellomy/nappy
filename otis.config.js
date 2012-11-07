b3ab2394704fcf5d7716e9d820a0c220e0a62283
bryn austin bellomy <bryn.bellomy@gmail.com>
2012-11-07 12:12:09 -0500

    tweaking otis doc generator config file

--- otis.config.js
+++ otis.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   inDir: "./",
   outDir: "../gh-pages",
-  index: "src/nappy.coffee.html",
+  index: "README.md.html",
   // delimStart: "#\\*",
   // delimEnd: "#\\*",
   markdownEngine: "showdown",
@@ -9,6 +9,6 @@ module.exports = {
   colourScheme: "friendly",
   ignoreHidden: true,
   sidebarState: true,
-  tolerant: true
-  // exclude: ''
+  tolerant: true,
+  exclude: 'otis.config.js,podspecs,Pods,Podfile,Podfile.lock,doc,node_modules,bin,src,lib,package.json,.git,.gitignore,Rakefile'
 };
